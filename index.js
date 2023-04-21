5c6b06365af7555d2f27bae853da04f1d91d76a5
erikpastuszek@gmail.com <erikpastuszek@gmail.com>
2023-04-21 20:21:51 -0300

    added everything cors

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const { Server } = require('socket.io');
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: ORIGIN,
+    origin: ORIGIN || '*',
   },
 });
 
