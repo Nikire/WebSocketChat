fcbd8b07708b8d7d644a82e889be29d4e2ad83df
Maximiliano Failla <faillamaximiliano@gmail.com>
2023-04-26 15:30:41 -0300

    User validate middleware

--- src/helpers/express.js
+++ src/helpers/express.js
@@ -22,4 +22,105 @@ module.exports = {
       next();
     });
   },
+  validateUser:
+    (type = '') =>
+    (req, res, next) => {
+      let { username, name, email, password } = req.body;
+      try {
+        // Type Validation
+        if (!type || type === '') {
+          return res
+            .status(400)
+            .json({ error: true, message: 'Type is required.' });
+        }
+        if (!['update', 'create'].includes(type)) {
+          return res
+            .status(400)
+            .json({ error: true, message: 'Invalid type.' });
+        }
+        switch (type) {
+          case 'create': {
+            if (!username || username === '') {
+              return res
+                .status(400)
+                .json({ error: true, message: 'Username is required.' });
+            }
+            if (!name || name === '') {
+              return res
+                .status(400)
+                .json({ error: true, message: 'Name is required.' });
+            }
+            if (!email || email === '') {
+              return res
+                .status(400)
+                .json({ error: true, message: 'Email is required.' });
+            }
+            if (!password || password === '') {
+              return res
+                .status(400)
+                .json({ error: true, message: 'Password is required.' });
+            }
+            break;
+          }
+        }
+        // Type check
+        if (typeof username !== 'string') {
+          return res
+            .status(400)
+            .json({ error: true, message: 'Username type is not valid.' });
+        }
+        if (typeof name !== 'string') {
+          return res
+            .status(400)
+            .json({ error: true, message: 'Name type is not valid.' });
+        }
+        if (typeof email !== 'string') {
+          return res
+            .status(400)
+            .json({ error: true, message: 'Email type is not valid.' });
+        }
+        if (typeof password !== 'string') {
+          return res
+            .status(400)
+            .json({ error: true, message: 'Password type is not valid.' });
+        }
+        // RegEx Validators
+        let usernameRegEx = /^[a-zA-Z0-9]{3,25}$/;
+        let passwordRegEx = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{5,30}$/;
+        let emailRegEx =
+          /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        let nameRegEx = /^[a-zA-Z ]{3,25}$/;
+
+        if (req.body.username && !usernameRegEx.test(req.body.username)) {
+          return res.status(400).json({
+            error: true,
+            message:
+              'Invalid username, please enter a valid username (must be valid characters and be 3-25 characters long)',
+          });
+        }
+        if (req.body.password && !passwordRegEx.test(req.body.password)) {
+          return res.status(400).json({
+            error: true,
+            message:
+              'Invalid password, it must have at least one number and one letter it also must be valid characters and be 5-30 characters long',
+          });
+        }
+        if (req.body.email && !emailRegEx.test(req.body.email)) {
+          return res.status(400).json({
+            error: true,
+            message: 'Invalid email, please enter a valid email',
+          });
+        }
+        if (req.body.name && !nameRegEx.test(req.body.name)) {
+          return res.status(400).json({
+            error: true,
+            message:
+              'Invalid name, please enter a valid name (must be just letters and 3-25 characters long)',
+          });
+        }
+        next();
+      } catch (error) {
+        return res.status(500).json({ error: true, message: error.message });
+      }
+    },
 };
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,8 +1,9 @@
 const { Router } = require('express');
 const router = Router();
 const { register, login } = require('../controllers/auth.controller.js');
+const { validateUser } = require('../helpers/express.js');
 
-router.post('/register', register);
+router.post('/register', validateUser('create'), register);
 router.post('/login', login);
 
 module.exports = router;
